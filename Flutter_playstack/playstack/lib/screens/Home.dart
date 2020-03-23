881751cf99f401c757503bd92c3d1d2134ca02ff
Enri <enri.ruiz.f@gmail.com>
2020-03-23 11:47:52 +0100

    Pantalla de home empezada

--- Flutter_playstack/playstack/lib/screens/Home.dart
+++ Flutter_playstack/playstack/lib/screens/Home.dart
@@ -17,6 +17,23 @@ class _HomeScreenState extends State<HomeScreen> {
           ], begin: Alignment.topLeft, end: FractionalOffset(0.3, 0.3)),
         ),
         child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              centerTitle: true,
+              title: Container(
+                  height: 40,
+                  width: 40,
+                  child: Image.asset('lib/assets/Photos/logo.png')),
+              shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(10.0),
+              ),
+              actions: <Widget>[
+                FlatButton.icon(
+                    onPressed: null,
+                    icon: Icon(CupertinoIcons.settings_solid),
+                    label: Text(''))
+              ],
+            ),
             backgroundColor: Colors.transparent,
             body: ListView(
               children: <Widget>[
@@ -25,20 +42,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Container(
-                              height: 40,
-                              width: 40,
-                              child: Image.asset('lib/assets/Photos/logo.png')),
-                          SizedBox(width: 70),
-                          FlatButton.icon(
-                              onPressed: null,
-                              icon: Icon(CupertinoIcons.settings_solid),
-                              label: Text('')),
-                        ],
-                      ),
                       Padding(
                         padding: const EdgeInsets.fromLTRB(10, 10, 0, 10),
                         child: Text(
@@ -59,7 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                   height: 130.0,
                                   width: 140.0,
                                   child: Image.asset(
-                                    'lib/assets/Photos/logo.png',
+                                    'lib/assets/Photos/pic1.png',
                                     fit: BoxFit.fitHeight,
                                   ),
                                 ),
@@ -77,6 +80,54 @@ class _HomeScreenState extends State<HomeScreen> {
                           },
                         ),
                       ),
+                      Container(
+                        height: 250.0,
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              'Recommendation',
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(1.0),
+                                fontSize: 23.0,
+                                fontFamily: 'Circular',
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(10.0),
+                            ),
+                            Container(
+                              height: 165.0,
+                              child: ListView.builder(
+                                itemCount: imageurl.length,
+                                scrollDirection: Axis.horizontal,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Column(
+                                    children: <Widget>[
+                                      SizedBox(
+                                        height: 130.0,
+                                        width: 140.0,
+                                        child: Image.asset(
+                                          imageurl[index],
+                                          fit: BoxFit.fitHeight,
+                                        ),
+                                      ),
+                                      Padding(padding: EdgeInsets.all(5.0)),
+                                      Text(
+                                        txt[index],
+                                        style: TextStyle(
+                                          color: Colors.white.withOpacity(1.0),
+                                          fontFamily: 'Circular',
+                                          fontSize: 10.0,
+                                        ),
+                                      )
+                                    ],
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
                     ],
                   ),
                 )
@@ -84,6 +135,17 @@ class _HomeScreenState extends State<HomeScreen> {
             )));
   }
 }
+
+List<String> imageurl = [
+  'lib/assets/Photos/pic1.png',
+  'lib/assets/Photos/i2.jpeg',
+  'lib/assets/Photos/i3.jpeg',
+];
+List<String> txt = [
+  'Curtain Call',
+  "The Eminem show",
+  'Greatest Hits',
+];
 
 /*
 return Scaffold(
