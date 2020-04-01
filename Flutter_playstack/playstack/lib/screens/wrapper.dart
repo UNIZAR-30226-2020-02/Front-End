b6954c94f1370a2bf2cdf633a92639b14bec8631
Andrew <aog25.2014@gmail.com>
2020-04-01 12:04:33 +0200

    Commit inicial Registro

--- Flutter_playstack/playstack/lib/screens/wrapper.dart
+++ Flutter_playstack/playstack/lib/screens/wrapper.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:playstack/screens/AccessScreen.dart';
+import 'package:playstack/screens/RegisterScreen.dart';
+import 'package:playstack/screens/mainscreen.dart';
 
 class Wrapper extends StatelessWidget {
   @override
@@ -8,10 +10,12 @@ class Wrapper extends StatelessWidget {
     final String user = null;
 
     // return either the Home or Authenticate widget
-    if (user == null) {
+    /*if (user == null) {
       return AccessScreen();
     } else {
       return AccessScreen();
     }
+    */
+     return RegisterScreen();
   }
 }
